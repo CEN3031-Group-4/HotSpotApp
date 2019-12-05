c7aa2e59b57c35bf4fe972e0fa2995185570953b
danielgiampaolo <danielgiampaolo@ufl.edu>
2019-12-05 00:45:08 -0500

    testing Heroku routes -DG

--- client/src/assets/nuclides.js
+++ client/src/assets/nuclides.js
@@ -1,7 +1,7 @@
 var Request = require("request");
 const dbNuclideOptions = []
 
-Request.get("http://localhost:5000/api/nuclides", (error, response, body) => {
+Request.get("https://localhost:5000/api/nuclides", (error, response, body) => {
         if(error) {
             return console.dir(error);
         }
