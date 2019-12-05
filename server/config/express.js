aea54a2ecd12d4aa30ad28bd84221a2e7bfd3440
danielgiampaolo <danielgiampaolo@ufl.edu>
2019-12-05 00:39:14 -0500

    testing Heroku routes -DG

--- server/config/express.js
+++ server/config/express.js
@@ -22,11 +22,8 @@ module.exports.init = () => {
     if (process.env.NODE_ENV === 'production') {
         // Exprees will serve up production assets
         app.use(express.static('client/build'));
-      
-        // Express serve up index.html file if it doesn't recognize route
-        const path = require('path');
-        app.get('*', (req, res) => {
-          res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        app.get('*', (request, response) => {
+            response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
         });
       }
 
